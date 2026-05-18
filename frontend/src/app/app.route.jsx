cbe24817720c0e7635449d3ff297fc8e92a05e82
Rahul Madeshiya <rahulmadeshiya851@gmail.com>
2026-05-18 19:00:35 +0530

    feat: add Home component with styling and routing; integrate Spline for visual effects

--- frontend/src/app/app.route.jsx
+++ frontend/src/app/app.route.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router";
 
 import Chat from "../features/chats/pages/Chat";
 
+import Home from "../features/home/pages/Home";
+
 import Login from "../features/auth/pages/Login";
 
 import AuthCallback from "../features/auth/pages/AuthCallback";
@@ -19,6 +21,14 @@ export const router = createBrowserRouter([
   },
   {
     path: "/",
+    element: (
+      <ProtectedRoute>
+        <Home />
+      </ProtectedRoute>
+    ),
+  },
+  {
+    path: "/chat",
     element: (
       <ProtectedRoute>
         <Chat />
